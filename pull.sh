0cb84b99faa47039b036e64577960bc8ee5d296f
Baruch Odem <baruchiro@gmail.com>
2020-10-17 23:00:17 +0300

    pull images also

--- pull.sh
+++ pull.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo systemctl stop home-server-compose.service
 git pull
+docker-compose pull
 sudo systemctl start home-server-compose.service
 journalctl -u home-server-compose.service -f
