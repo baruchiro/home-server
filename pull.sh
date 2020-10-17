f765a5fadb142f9b0cef09e843e361f3152da5cd
Baruch Odem <baruchiro@gmail.com>
2020-10-17 23:15:28 +0300

    docker image prune -f

--- pull.sh
+++ pull.sh
@@ -3,4 +3,5 @@ sudo systemctl stop home-server-compose.service
 git pull
 docker-compose pull
 sudo systemctl start home-server-compose.service
+docker image prune -f
 journalctl -u home-server-compose.service -f
