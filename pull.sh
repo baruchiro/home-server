d93745e271c2003fddac17fd2e73fbca5f023721
Baruch Odem <baruchiro@gmail.com>
2021-02-19 12:50:18 +0200

    seed ARGs

--- pull.sh
+++ pull.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo systemctl stop home-server-compose.service
+seed/init.sh
 git pull
 docker-compose pull
 sudo systemctl start home-server-compose.service
 docker image prune -f
+docker volume prune -f
 journalctl -u home-server-compose.service -f
