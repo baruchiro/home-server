9aef95be5dc171d600deeef1337e0dee51cb94ce
Baruch Odem (Rothkoff) <baruchiro@gmail.com>
2021-04-01 16:43:39 +0300

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 sudo systemctl stop home-server-compose.service
 git pull
-seed/init.sh
+# seed/init.sh
 # docker-compose -f docker-compose.yml -f docker-compose.mongo.yml build --no-cache mongo-seed
-docker-compose -f docker-compose.yml -f docker-compose.mongo.yml build mongo-seed
-docker-compose -f docker-compose.yml -f docker-compose.mongo.yml pull
+# docker-compose -f docker-compose.yml -f docker-compose.mongo.yml build mongo-seed
+# docker-compose -f docker-compose.yml -f docker-compose.mongo.yml pull
+docker-compose -f docker-compose.yml pull
 sudo systemctl start home-server-compose.service
 docker image prune -f
 journalctl -u home-server-compose.service -f
