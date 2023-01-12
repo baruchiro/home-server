5d7b42b751288a1b59ca852e01b24aa91107f97e
Baruch Odem (Rothkoff) <baruchiro@gmail.com>
2023-01-12 18:44:29 +0200

    don't prune volumes

--- pull.sh
+++ pull.sh
@@ -3,5 +3,4 @@ sudo systemctl stop home-server-compose.service
 git pull
 docker-compose pull
 sudo systemctl start home-server-compose.service
-docker system prune --volumes -f
 journalctl -u home-server-compose.service -f
