c276ed642e14f98ed258247514ba29b37748bb44
Baruch Odem <baruchiro@gmail.com>
2025-01-02 04:29:10 +0000

    fixup! Update home-server service and scripts for Docker Swarm deployment

--- fix-rclone.sh
+++ fix-rclone.sh
@@ -1,7 +1,8 @@
 # https://forum.rclone.org/t/docker-plugin-fail-to-start-reinstall/29871/8
 
-docker volume rm home-server_paperless-export
-docker volume rm home-server_mysql-backup
+docker volume rm paperless_paperless-export
+docker volume rm paperless_paperless-consume
+docker volume rm paperless_mysql-backup
 
 docker plugin disable rclone
 sudo rm /var/lib/docker-plugins/rclone/cache/docker-plugin.state
