6a601dfebae43c53a72c2715cd3c3227363d0b37
Baruch Odem <baruchiro@gmail.com>
2025-05-19 06:23:43 +0000

    Remove Twitter Followers sensor from Home Assistant configuration and add new sensors for GitHub stars tracking

--- network/update.sh
+++ network/update.sh
@@ -1,6 +1,5 @@
 cp .env *.env network/
 cd network
-sudo systemctl stop network-home-server.service
 docker-compose pull
-sudo systemctl start network-home-server.service
+sudo systemctl restart network-home-server.service
 journalctl -u network-home-server.service -f
